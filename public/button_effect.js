a14bc2a51fd5e15ac461a46b49f0c190e0d54492
GMM-rgb <maximus.farvour@outlook.com>
2025-01-23 08:32:36 +0000

    source update 44

--- public/button_effect.js
+++ public/button_effect.js
@@ -8,41 +8,45 @@ const effectWait2 = '450';
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-clearChatButton.addEventListener('mouseover', pushButtonDownVisual => {
-    if(clearChatButton.style.boxShadow === '0 4.25px 0px rgb(100, 100, 100)' || clearChatButton.style.boxShadow === '' || clearChatButton.style.transform === 'translateY(0px)' || clearChatButton.style.transform === '') {
-        clearChatButton.style.boxShadow = '0 2.25px 0px rgb(100, 100, 100)';
-        clearChatButton.style.transform = 'translateY(2px)';
-    wait(effectWait1).then(() => {
-        clearChatButton.style.border = '3px solid rgb(0, 234, 255)';
-        wait(effectWait2).then(() => {
-            clearChatButton.style.borderTop = '3px solid rgb(179, 179, 180)';
-            clearChatButton.style.borderBottom = '3px solid rgb(179, 179, 180)';
-            clearChatButton.style.borderLeft = '3px solid rgb(179, 179, 180)';
-            clearChatButton.style.borderRight = '3px solid rgb(179, 179, 180)';
-        });
-    });
-    } else {
-        return;
-    }
-});
-clearChatButton.addEventListener('mouseout', revertPushButtonDownVisual => {
-    if(clearChatButton.style.boxShadow === '0 2.25px 0px rgb(100, 100 ,100)' || clearChatButton.style.boxShadow === '' || clearChatButton.style.transform === 'translateY(2px)' || clearChatButton.style.transform === '') {
-        clearChatButton.style.boxShadow = '0 4.25px 0px rgb(100, 100, 100)';
-        clearChatButton.style.transform = 'translateY(0px)';
-    } else {
-        return;
-    }
-});
 
-function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+// Function to add effects to a button
+function addButtonEffects(button) {
+    button.addEventListener('mouseover', () => {
+        if(button.style.boxShadow === '0 4.25px 0px rgb(100, 100, 100)' || button.style.boxShadow === '' || button.style.transform === 'translateY(0px)' || button.style.transform === '') {
+            button.style.boxShadow = '0 2.25px 0px rgb(100, 100, 100)';
+            button.style.transform = 'translateY(2px)';
+            wait(effectWait1).then(() => {
+                button.style.border = '3px solid rgb(0, 234, 255)';
+                wait(effectWait2).then(() => {
+                    button.style.borderTop = '3px solid rgb(179, 179, 180)';
+                    button.style.borderBottom = '3px solid rgb(179, 179, 180)';
+                    button.style.borderLeft = '3px solid rgb(179, 179, 180)';
+                    button.style.borderRight = '3px solid rgb(179, 179, 180)';
+                });
+            });
+        } else {
+            return;
+        }
+    });
+
+    button.addEventListener('mouseout', () => {
+        if(button.style.boxShadow === '0 2.25px 0px rgb(100, 100 ,100)' || button.style.boxShadow === '' || button.style.transform === 'translateY(2px)' || button.style.transform === '') {
+            button.style.boxShadow = '0 4.25px 0px rgb(100, 100, 100)';
+            button.style.transform = 'translateY(0px)';
+        } else {
+            return;
+        }
+    });
+
+    button.addEventListener('click', () => {
+        button.style.border = '3px solid rgb(0, 234, 255)';
+        if(button.style.boxShadow === '0 2.25px 0px rgb(100, 100, 100)' || button.style.boxShadow === '' || button.style.transform === 'translateY(3px)' || button.style.transform === '') {
+            button.style.boxShadow = '0 1.25px 0px rgb(100, 100, 100)';
+        }
+    });
 }
-clearChatButton.addEventListener('click', highlightButton => {
-    clearChatButton.style.border = '3px solid rgb(0, 234, 255)';
-    if(clearChatButton.style.boxShadow === '0 2.25px 0px rgb(100, 100, 100)' || clearChatButton.style.boxShadow === '' || clearChatButton.style.transform === 'translateY(3px)' || clearChatButton.style.transform === '') {
-        clearChatButton.style.boxShadow = '0 1.25px 0px rgb(100, 100, 100)';
-    }
-});
+
+// Apply effects to both buttons
+addButtonEffects(clearChatButton);
+addButtonEffects(saveButtonC);
+addButtonEffects(uploadButton);
